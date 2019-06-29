c98e90d8d8d24825817f6418aa7d7d604945241d
lau <laura.javiera77@gmail.com>
2019-06-29 18:39:00 -0400

    funcion md-link y algunas promesas

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const path = require("path");
+const pathNode= require("path");
 const fs = require("fs");//exportando fs
 const marked= require('marked');//exportando marked que es el que extrae link
 const util = require('util');
@@ -9,7 +9,7 @@ process.argv.forEach((option, index) => {//vemos posiciones de lo que escribe el
 });
 
 let path_to_file = process.argv[2];//ruta, process.argv llama lo que escribe el usuario en consola
-path_to_file = path.resolve(path_to_file);// la vuelve absoluta
+path_to_file = pathNode.resolve(path_to_file);// la vuelve absoluta
 let option_1 = process.argv[3];//deberi ser validate
 let option_2 = process.argv[4];//deberia ser stats
 let stats = false;
@@ -59,34 +59,45 @@ const isDirectory = async path_to_file=>{
   pathUser(path_to_file);
 
 //funcion f.s para leer archivo.md
- const links = (path) =>{//llamamos la variable que contiene la ruta
-    fs.readFile(path,"utf8", (err,data) =>{
-      console.log("Ruta o archivo md", path);
-      if(err){
-        throw err;
-      }
-      let links =[];
-  
-      const renderer = new marked.Renderer();//renderer es un metodo de marked
-  
-      renderer.link = function(href, title, text){//link tb es metodo de marked
-  
-        links.push({//ponemos dentro de la const links los elementos que necesitamos
-          
-          href:href,
-          text:text,
-          file:path_to_file
+
+const links = (path) =>{//llamamos la variable que contiene la ruta
+  if(pathNode.extname(path) != ".md"){//El método path.extname () devuelve la extensión de una ruta de archivo. entonces si es distinto a md tira error
+    console.log("Es archivo pero no .md")  
+ }else{
+ return new Promise((resolve,reject)=>{// convertimos en promesa
+  fs.readFile(path,"utf8", (err,data) =>{
+    if(err){ 
+      reject(err); 
+ }else{
+     console.log("Ruta o archivo md", path);
+   
+       let links =[];
+
+         const renderer = new marked.Renderer();//renderer es un metodo de marked
+
+            renderer.link = function(href, title, text){//link tb es metodo de marked
+
+             links.push({//ponemos dentro de la const links los elementos que necesitamos
         
-        })
-  
-      }
-      marked(data, {renderer:renderer})
-      linkValidate(links)
-      linkStats(links) 
-    })
-  //console.log(links('./readme.md'));
+             href:href,
+             text:text,
+             file:path_to_file
+      
+             })
+
+            }
+    marked(data, {renderer:renderer})
+    resolve(links)
+    linkValidate(links)
+    linkStats(links) 
   }
-  
+})
+
+
+})
+}
+
+}
    
 
  
@@ -142,25 +153,68 @@ const isDirectory = async path_to_file=>{
   }
 
   function linkStats(links) {
+    let countLink =[];
        if(stats===true){
-      let countLink= links.length;
+     countLink= links.length;
       console.log('Existen',countLink, 'link en tu .md');
+      let cont={};
       links.forEach(el=>{
+        let statusCount ={};
         fetch(el.href)
         .then(res=>{
-          let rest = res.status;
-          let countValidate = rest.length;
-          console.log('El total de link validados es',countValidate);
-        })
+          let stats= res.status
+          console.log('este es', stats)
+          cont= stats.length;
+          console.log('cont aqui',cont);
+         })
+          // statusCount = statusCount.length;
+          
+          // }
+       
         .catch(err=>{
           let countErr = err.length;
-          console.log(countErr);
+         // console.log(countErr);
         })
       })
+     
        }
     
 
     
   }
   
-    
+  /** aqui llamo a la funcion general md-link, convierto en promesas el resto me queda la duda
+   *  con option ya que nose donde se usa, me falta hacer los export y los test.
+   * 
+   */
+         
+const mdLinks = (path, options) => {
+  return new Promise((resolve, reject)=>{
+       isDirectory(path)
+            .then(res=>{
+                 let isDir = res;
+                 if(isDir){
+                      console.log("directorio")
+                     resolve(getMdFilehound(path)); 
+                 }if(isDir===false){
+                       console.log("archivo");
+                       resolve(links(path))
+
+                     }
+               
+                     links(path)
+                      .then(res=>{
+                         resolve(linkValidate(res)||linkStats(res));
+
+                      })
+                      .catch(err=>{
+                        reject(err);
+                      })
+                     
+                 })
+
+            
+           
+ })
+}
+mdLinks(path_to_file);
--- tallerPromesas.js
+++ tallerPromesas.js
@@ -21,3 +21,104 @@ readAFiles("README.md", "utf-8")
 .catch(err =>{
     console.log(err);
 })
+
+
+// // funcion que lee archivo y extrae los links
+const links = (path) =>{
+  if(pathNode.extname(path) != ".md"){
+     console.log("Es archivo pero no .md")  
+  }else{
+  return new Promise((resolve,reject)=>{
+      
+       fs.readFile(path,"utf-8", (err,data) =>{
+            if(err){ 
+                 reject(err); 
+            }else{
+                 let links =[];   
+                 const renderer = new marked.Renderer();
+                 renderer.link = function(href, title, text){
+                      links.push({
+                           href:href,
+                           text:text,
+                           file:path
+                      }) 
+                 }
+                 marked(data, {renderer:renderer});
+               
+
+                 resolve(links);        
+                 
+                 
+                 
+            }
+       })
+  
+      
+  })
+}
+       
+}
+
+const links = (path) =>{//llamamos la variable que contiene la ruta
+  if(path.extname(path) != ".md"){//El método path.extname () devuelve la extensión de una ruta de archivo. entonces si es distinto a md tira error
+    console.log("Es archivo pero no .md")  
+ }else{
+ return new Promise((resolve,reject)=>{// convertimos en promesa
+  fs.readFile(path,"utf8", (err,data) =>{
+    if(err){ 
+      reject(err); 
+ }else{
+     console.log("Ruta o archivo md", path);
+   
+       let links =[];
+
+         const renderer = new marked.Renderer();//renderer es un metodo de marked
+
+            renderer.link = function(href, title, text){//link tb es metodo de marked
+
+             links.push({//ponemos dentro de la const links los elementos que necesitamos
+        
+             href:href,
+             text:text,
+             file:path_to_file
+      
+             })
+
+            }
+    marked(data, {renderer:renderer})
+    resolve(links)
+    linkValidate(links)
+    linkStats(links) 
+  }
+})
+
+
+})
+}
+
+}
+
+function linkStats(links) {
+  if(stats===true){
+ var countLink= links.length;
+ console.log('Existen',countLink, 'link en tu .md');
+ let countValidate = 0;
+ links.forEach(el=>{
+   fetch(el.href)
+   .then(res=>{
+    //  console.log(res);
+     countValidate = countValidate + 1;
+    // console.log('El total de link validados es',countValidate);
+   })
+   .catch(error => {
+    console.log('aqui el  error',error);
+    //let countErr = err.length;
+    //console.log(countErr);
+   });
+ })
+ console.log("aqui va countvalidate",countValidate);
+  }
+
+
+
+}
